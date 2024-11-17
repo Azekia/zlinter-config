a479b69d1846838275d84b1f1fcb79f00ebae90b
Antonio Begines <antonio@beegines.net>
2024-11-17 18:46:29 +0100

    fix typos

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// forked from airbnb-base
-module.exports = {
+// forked from airbnb-base, but not imposing unneeded dependencies
+const eslintConfig =  {
     extends: [
       './rules/best-practices',
-      './rules/errors',
-      './rules/node',
-      './rules/style',
-      './rules/variables',
-      './rules/es6',
-      './rules/imports',
-      './rules/strict',
+      './rules/errors.js',
+      './rules/node.js',
+      './rules/style.js',
+      './rules/variables.js',
+      './rules/es6.js',
+      './rules/imports.js',
+      './rules/strict.js',
     ].map(require.resolve),
     parserOptions: {
       ecmaVersion: 2018,
@@ -16,4 +16,5 @@ module.exports = {
     },
     rules: {},
   };
-  
+console.log(eslintConfig);
+module.exports = eslintConfig;
