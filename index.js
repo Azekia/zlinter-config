47f74e50fce8592a99bd655e4721848043c77b5d
Antonio Begines <antonio@beegines.net>
2024-11-17 14:39:10 +0100

    forked from airbna-base

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const prettier_config = require('./config_prettier.js');
-
-module.exports = prettier_config;
+// forked from airbnb-base
+module.exports = {
+    extends: [
+      './rules/best-practices',
+      './rules/errors',
+      './rules/node',
+      './rules/style',
+      './rules/variables',
+      './rules/es6',
+      './rules/imports',
+      './rules/strict',
+    ].map(require.resolve),
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    rules: {},
+  };
+  
